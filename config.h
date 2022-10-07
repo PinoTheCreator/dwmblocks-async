e3d2bd74b8b6b895088bf33b360a711b766f2f29
Carlo Aspren Gines Lara <101257700+PinoTheCreator@users.noreply.github.com>
2022-10-07 18:39:56 +0200

    all blocks now point to dotfile

--- config.h
+++ config.h
@@ -1,4 +1,4 @@
-#define CMDLENGTH 45
+#define CMDLENGTH 100
 #define DELIMITER " "
 #define CLICKABLE_BLOCKS true
 
@@ -6,10 +6,12 @@
 
 const Block blocks[] = {
   /*    command   updatetime  signal*/
-        BLOCK(DR "music", 1, 15),
-  BLOCK(DR "packup", 18000, 16),
+        //BLOCK(DR "music", 1, 15),
+        BLOCK(DR "packup", 18000, 16),
         //BLOCK(DR "weather", 3600, 11),
         BLOCK(DR "vol", 0, 13),
+        BLOCK(DR "memory", 10, 0),
+        BLOCK(DR "cpu", 5, 18),
         BLOCK(DR "temp", 20, 12),
         BLOCK(DR "bat", 180, 14),
         BLOCK(DR "calendar", 1, 17),
