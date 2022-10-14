84d958b15980dbd0eeb933a680490b477c83184f
Carlo Aspren Gines Lara <101257700+PinoTheCreator@users.noreply.github.com>
2022-10-14 17:07:13 +0200

    added wifi module works with networkmanager

--- config.h
+++ config.h
@@ -10,9 +10,10 @@ const Block blocks[] = {
         BLOCK(DR "packup", 18000, 16),
         //BLOCK(DR "weather", 3600, 11),
         BLOCK(DR "vol", 0, 13),
-        BLOCK(DR "memory", 10, 0),
+        BLOCK(DR "internet", 20, 20),
+        BLOCK(DR "memory", 10, 19),
         BLOCK(DR "cpu", 5, 18),
         BLOCK(DR "temp", 20, 12),
         BLOCK(DR "bat", 180, 14),
-        BLOCK(DR "calendar", 1, 17),
+        BLOCK(DR "date", 1, 17),
 };
